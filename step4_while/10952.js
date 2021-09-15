5d8172af0d28c96c6b5cd811a553d67c402180b1
minjuice1 <minjuice93@gmail.com>
2021-09-15 15:39:10 +0900

    10952.js

--- step4_while/10952.js
+++ step4_while/10952.js
@@ -3,7 +3,9 @@
 // 예제 출력 : 2, 5, 7, 17, 7
 
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('/n');
+let input = fs.readFileSync('/input.txt').toString().split('/n');
 let a = Number(input[0]);
 let b = Number(input[1]);
 console.log(input);
+
+
