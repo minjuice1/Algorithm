7f29a930c3356ccd8e856da2233e5d9ba1793612
minjuice1 <minjuice93@gmail.com>
2021-09-15 17:23:47 +0900

    Change dir and Add txt files

--- step4_while/10952/app.js
+++ step4_while/10952/app.js
@@ -3,9 +3,12 @@
 // 예제 출력 : 2, 5, 7, 17, 7
 
 let fs = require('fs');
-let input = fs.readFileSync('/input.txt').toString().split('/n');
-let a = Number(input[0]);
-let b = Number(input[1]);
+let input = fs.readFileSync('./input.txt').toString().split('\n');
 console.log(input);
+
+let a = Number(input[0]);
+console.log(a);
+let b = Number(input[1]);
+console.log(b);
 
 
