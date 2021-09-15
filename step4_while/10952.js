ff5a74b330a9c6a7755cd2dddcc34d78b8bb5d7b
minjuice1 <minjuice93@gmail.com>
2021-09-15 15:27:35 +0900

    Change folder names

---
+++ step4_while/10952.js
@@ -0,0 +1,9 @@
+// 각 테스트 케이스마다 A+B를 출력한다.
+// 예제 입력 : 1 1 , 2 3 , 3 4 , 9 8 , 5 2 , 0 0
+// 예제 출력 : 2, 5, 7, 17, 7
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('/n');
+let a = Number(input[0]);
+let b = Number(input[1]);
+console.log(input);
