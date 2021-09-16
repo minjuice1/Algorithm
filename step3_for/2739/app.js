99fd75035aa3593982bb43673682a52effbfe114
minjuice1 <minjuice93@gmail.com>
2021-09-16 22:46:10 +0900

    2739.js

---
+++ step3_for/2739/app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// const input = fs.readFileSync('./input.txt').toString().split('\n');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(file).toString().split('\n');
+
+input = input[0];
+// input = input.split(' ').map((item) => +item);
+input = input.split(' ').map((item) => Number(item));
+
+solution(input[0], input[1]);
+
+function solution(A) {
+  for(i = 1; i <= 9; i++){
+    console.log(A + ' * ' + i + ' = ' + (A * i));
+  }
+}
