4ec3b44065282e1e040724b5d0131ff0d59623df
minjuice1 <minjuice93@gmail.com>
2021-09-17 16:29:00 +0900

    11022.js

---
+++ step3_for/11022/app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(file).toString().split('\n');
+
+let result = '';
+for (let i = 1; i <= input[0]; i++){
+  let numbers = input[i].split(' ');
+  result += "Case #" + i +": " + Number(numbers[0]) +" + "+ Number(numbers[1]) +" = " + (Number(numbers[0]) + Number(numbers[1])) + '\n';
+  
+}
+console.log(result);
