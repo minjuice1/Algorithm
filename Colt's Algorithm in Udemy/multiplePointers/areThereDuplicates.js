16d98d0b0f8f649fb57e1ed0738e1c823140daf2
minjuice1 <minjuice93@gmail.com>
2022-06-07 12:21:46 +0900

    다중 포인터, areThereDuplicates 코드 리팩토링

--- Colt's Algorithm in Udemy/multiplePointers/areThereDuplicates.js
+++ Colt's Algorithm in Udemy/multiplePointers/areThereDuplicates.js
@@ -5,18 +5,8 @@
 //   return new Set(arguments).size!== arguments.length;
 // }
 
-function areThereDuplicates(...args) {
-  args.sort((a,b) => a > b);
-  let start = 0;
-  let next = 1;
-  while(next < args.length) {
-    if(args[start] === args[next]) {
-      return true
-    }
-    start++
-    next++
-  }
-  return false
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
 }
 
 
