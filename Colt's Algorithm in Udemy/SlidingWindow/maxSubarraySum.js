b83101ecbf8f76f764abef23858801f1ae09ae72
minjuice1 <minjuice93@gmail.com>
2022-06-12 16:52:28 +0900

    Sliding Window - minSubArrayLen

--- Colt's Algorithm in Udemy/SlidingWindow/maxSubarraySum.js
+++ Colt's Algorithm in Udemy/SlidingWindow/maxSubarraySum.js
@@ -12,10 +12,10 @@ function maxSubarraySum(arr, num) {
 	for (let i = 0; i < num; i++) {
 		total += arr[i];
 	}
-
 	let currentTotal = total;
 	for (let i = num; i < arr.length; i++) {
 		currentTotal += arr[i] - arr[i - num];
+		total = Math.max(total, currentTotal);
 	}
 	return total;
 }
