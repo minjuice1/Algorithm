013d89adb0d6cd62f143b21ad5f665be44a3be5c
minjuice1 <minjuice93@gmail.com>
2022-07-25 21:25:37 +0900

    DoublyLinkedList : push, pop, shift, unshift, get, set, insert, remove 메서드 구현.

---
+++ Colt's Algorithm in Udemy/LinkedList/doublyLinkedLists.js
@@ -0,0 +1,139 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+		this.prev = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(val) {
+		let newNode = new Node(val);
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+	unshift(val) {
+		let newNode = new Node(val);
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.head.prev = newNode;
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		this.length++;
+		return this;
+	}
+	pop() {
+		if (this.length === 0) return undefined;
+		let poppedNode = this.tail;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.tail = poppedNode.prev;
+			this.tail.next = null;
+			poppedNode.prev = null;
+		}
+		this.length--;
+		return poppedNode;
+	}
+	shift() {
+		if (this.length === 0) return undefined;
+		let oldHead = this.head;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.head = oldHead.next;
+			this.head.prev = null;
+			oldHead.next = null;
+		}
+		this.length--;
+		return oldHead;
+	}
+	get(index) {
+		if (index < 0 || index >= this.length) return null;
+		let count, current;
+		if (index <= this.length / 2) {
+			count = 0;
+			current = this.head;
+			while (count !== index) {
+				current = current.next;
+				count++;
+			}
+		} else {
+			count = this.length - 1;
+			current = this.tail;
+			while (count !== index) {
+				current = current.prev;
+				count--;
+			}
+		}
+		return current;
+	}
+	set(index, val) {
+		let foundNode = this.get(index);
+		if (foundNode) {
+			foundNode.val = val;
+			return true;
+		}
+		return false;
+	}
+	insert(index, val) {
+		if (index < 0 || index > this.length) return null;
+		if (index === 0) return this.unshift(val);
+		if (index === this.length) return this.push(val);
+
+		let newNode = new Node(val);
+		let beforeNode = this.get(index - 1);
+		let afterNode = beforeNode.next;
+		beforeNode.next = newNode;
+
+		newNode.prev = beforeNode;
+		newNode.next = afterNode;
+		afterNode.prev = newNode;
+		this.length++;
+		return true;
+	}
+	remove(index) {
+		if (index < 0 || index >= this.length) return false;
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+
+		let removeNode = this.get(index);
+		let beforeNode = removeNode.prev;
+		let afterNode = removeNode.next;
+
+		beforeNode.next = afterNode;
+		afterNode.prev = beforeNode;
+		removeNode.next = null;
+		removeNode.prev = null;
+		this.length--;
+		return removeNode;
+	}
+}
+
+let list = new DoublyLinkedList();
+
+list.push("HI");
+list.push("THERE");
+list.push("!!!");
+list.push("HOW");
+list.push("ARE");
+list.push("YOU");
+list.push("?");
